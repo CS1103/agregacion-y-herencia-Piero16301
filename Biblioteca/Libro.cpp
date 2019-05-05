ad69c6cfe45eaf5e4ae1ae733a54d2a7d64f72a2
Piero <piero.morales@utec.edu.pe>
2019-05-04 22:07:38 -0500

    Actualizacion

--- Biblioteca/Libro.cpp
+++
@@ -1,7 +0,0 @@
-#include "Libro.h"
-
-Libro::Libro(const string &nombre, int numVolumen, int numLibro) : Volumen(nombre, numVolumen), numLibro(numLibro) {}
-
-void Libro::setNombre() {
-
-}
--- Biblioteca/Revista.cpp
+++
@@ -1,4 +0,0 @@
-#include "Revista.h"
-
-Revista::Revista(const string &nombre, int numVolumen, int numRevista) : Volumen(nombre, numVolumen),
-                                                                         numRevista(numRevista) {}
